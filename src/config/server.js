9596038dc70514a403ce739a0f4ecb023a740a90
walttoncoelho <walltton@outlook.com>
2022-02-15 17:37:43 -0300

    config cours

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const cors = require('./cors');
 
 // Todas as requisições passarão pelo bodyParser
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cors);
 
 server.listen(port, function() {
   console.log('Server listening on port ' + port);
