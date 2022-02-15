e72f157fba611fd77456cbc4efb61e62f2594407
walttoncoelho <walltton@outlook.com>
2022-02-15 10:17:19 -0300

    inicianddo serviddor

---
+++ src/config/server.js
@@ -0,0 +1,13 @@
+const port = process.env.PORT || 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+// Todas as requisições passarão pelo bodyParser
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(port, function() {
+  console.log('Server listening on port ' + port);
+}   );
